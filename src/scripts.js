e1cec5d0f5d639833d3c7a4923078b24ed2904f0
chicken-1 <nguyenthanhan3703@gmai.com>
2025-05-20 21:14:19 +0700

    update UI

--- src/scripts.js
+++ src/scripts.js
@@ -36,3 +36,37 @@ window.onclick = function(event) {
     }
 }
 
+const navContent = document.getElementById('nav-content');
+
+function displayNavContent(contentType){
+    switch (contentType) {
+        case 'test':
+            fetch('test.html')
+                .then(response => {
+                    if(!response.ok) {
+                        throw new Error('Network response bad');
+                    }
+                    return response.text();
+                })
+                .then(html => {
+                    navContent.innerHTML = html;
+                })
+                .catch(error => {
+                    navContent.innerHTML = '<p> Fail to load test content.</p>';
+                    console.error('Error loading test/html', error);
+                });
+            break;
+        default:
+            navContent.innerHTML = '<p> Hello.</p>';
+    }
+}
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('active'));
+        event.target.classList.add('active');
+        const contentType = event.target.getAttribute('data-content');
+        displayNavContent(contentType);
+    });
+});
